096818b5310d90207225f9c0ac15bdc6c975b1aa
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2019-07-12 00:26:05 -0300

    Initial Commit

---
+++ Minhas Anotações/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minhas Anotações
+//
+//  Created by Ytallo on 12/07/19.
+//  Copyright © 2019 CursoiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
