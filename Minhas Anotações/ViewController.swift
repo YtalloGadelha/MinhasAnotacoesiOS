c13c0820471dc1907db3b17bb9ddf4b118698fba
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2019-07-24 22:59:04 -0300

    update

--- Minhas Anotações/ViewController.swift
+++ Minhas Anotações/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textoCampo: UITextView!
+    let chave = "minhaAnotacao"
+    
+    @IBAction func salvarAnotacao(_ sender: Any) {
+        
+        if let texto = textoCampo.text {
+            
+            UserDefaults.standard.set(texto, forKey: chave)
+        }
+    }
+    
+    func recuperarAnotacao()-> String{
+        
+        if let textoRecuperado = UserDefaults.standard.object(forKey: chave){
+            
+            return textoRecuperado as! String
+        }
+        
+        return ""
+    }
+    
+//    override var prefersStatusBarHidden: Bool{
+//        return true
+//    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textoCampo.text = recuperarAnotacao()
     }
-
-
 }
 
